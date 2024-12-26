d958ef6b6e3867d9f856f611bbbdb18fbdfbd105
SWC <15207165701@163.com>
2024-12-26 16:47:38 +0800

    init

---
+++ rcdw_fsod/modeling/meta_arch/decoupleRCN.py
@@ -0,0 +1,167 @@
+import torch
+import logging
+from torch import nn
+from detectron2.structures import ImageList
+from detectron2.utils.logger import log_first_n
+from detectron2.modeling.backbone import build_backbone
+from detectron2.modeling.postprocessing import detector_postprocess
+from detectron2.modeling.proposal_generator import build_proposal_generator
+from .build import META_ARCH_REGISTRY
+from defrcn.modeling.roi_heads import build_roi_heads
+import torch.nn as nn
+from torch.autograd import Function
+
+__all__ = ["GeneralizedRCNN"]
+
+
+class GradientDecoupleLayer(Function):
+
+    @staticmethod
+    def forward(ctx, x, _lambda):
+        ctx._lambda = _lambda
+        return x
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        grad_output = grad_output * ctx._lambda
+        return grad_output, None
+
+
+class AffineLayer(nn.Module):
+    def __init__(self, num_channels, bias=False):
+        super(AffineLayer, self).__init__()
+        weight = torch.FloatTensor(1, num_channels, 1, 1).fill_(1)
+        self.weight = nn.Parameter(weight, requires_grad=True)
+
+        self.bias = None
+        if bias:
+            bias = torch.FloatTensor(1, num_channels, 1, 1).fill_(0)
+            self.bias = nn.Parameter(bias, requires_grad=True)
+
+    def forward(self, X):
+        out = X * self.weight.expand_as(X)
+        if self.bias is not None:
+            out = out + self.bias.expand_as(X)
+        return out
+
+
+def decouple_layer(x, _lambda):
+    return GradientDecoupleLayer.apply(x, _lambda)
+
+
+class AttentionWeight(torch.nn.Module):
+    def __init__(self, channels=None, e_lambda=1e-4):
+        super(AttentionWeight, self).__init__()
+
+        self.activaton = nn.Sigmoid()
+        self.e_lambda = e_lambda
+
+    def __repr__(self):
+        s = self.__class__.__name__ + '('
+        s += ('lambda=%f)' % self.e_lambda)
+        return s
+
+    @staticmethod
+    def get_module_name():
+        return "simam"
+
+    def forward(self, x):
+        b, c, h, w = x.size()
+
+        n = w * h - 1
+
+        x_minus_mu_square = (x - x.mean(dim=[2, 3], keepdim=True)).pow(2)
+        y = x_minus_mu_square / (4 * (x_minus_mu_square.sum(dim=[2, 3], keepdim=True) / n + self.e_lambda)) + 0.5
+
+        return x * self.activaton(y)
+
+
+@META_ARCH_REGISTRY.register()
+class GeneralizedRCNN(nn.Module):
+
+    def __init__(self, cfg):
+        super().__init__()
+
+        self.cfg = cfg
+        self.device = torch.device(cfg.MODEL.DEVICE)
+        self.backbone = build_backbone(cfg)
+        self._SHAPE_ = self.backbone.output_shape()
+        self.proposal_generator = build_proposal_generator(cfg, self._SHAPE_)
+        self.roi_heads = build_roi_heads(cfg, self._SHAPE_)
+        self.normalizer = self.normalize_fn()
+        self.affine_rpn = AffineLayer(num_channels=self._SHAPE_['res4'].channels, bias=True)
+        self.affine_rcnn = AffineLayer(num_channels=self._SHAPE_['res4'].channels, bias=True)
+        self.attentionWeight = AttentionWeight()
+        self.to(self.device)
+
+        if cfg.MODEL.BACKBONE.FREEZE:
+            for p in self.backbone.parameters():
+                p.requires_grad = False
+            print("froze backbone parameters")
+
+        if cfg.MODEL.RPN.FREEZE:
+            for p in self.proposal_generator.parameters():
+                p.requires_grad = False
+            print("froze proposal generator parameters")
+
+        if cfg.MODEL.ROI_HEADS.FREEZE_FEAT:
+            for p in self.roi_heads.res5.parameters():
+                p.requires_grad = False
+            print("froze roi_box_head parameters")
+
+    def forward(self, batched_inputs):
+        if not self.training:
+            return self.inference(batched_inputs)
+        assert "instances" in batched_inputs[0]
+        gt_instances = [x["instances"].to(self.device) for x in batched_inputs]
+        proposal_losses, detector_losses, _, _ = self._forward_once_(batched_inputs, gt_instances)
+        losses = {}
+        losses.update(detector_losses)
+        losses.update(proposal_losses)
+        return losses
+
+    def inference(self, batched_inputs):
+        assert not self.training
+        _, _, results, image_sizes = self._forward_once_(batched_inputs, None)
+        processed_results = []
+        for r, input, image_size in zip(results, batched_inputs, image_sizes):
+            height = input.get("height", image_size[0])
+            width = input.get("width", image_size[1])
+            r = detector_postprocess(r, height, width)
+            processed_results.append({"instances": r})
+        return processed_results
+
+    def _forward_once_(self, batched_inputs, gt_instances=None):
+
+        images = self.preprocess_image(batched_inputs)
+        features_begin = self.backbone(images.tensor)
+        features = {k: self.attentionWeight(features_begin[k]) for k in features_begin}
+
+        features_de_rpn = features
+        if self.cfg.MODEL.RPN.ENABLE_DECOUPLE:
+            scale = self.cfg.MODEL.RPN.BACKWARD_SCALE
+            features_de_rpn = {k: self.affine_rpn(decouple_layer(features[k], scale)) for k in features}
+        proposals, proposal_losses = self.proposal_generator(images, features_de_rpn, gt_instances)
+
+        features_de_rcnn = features
+        if self.cfg.MODEL.ROI_HEADS.ENABLE_DECOUPLE:
+            scale = self.cfg.MODEL.ROI_HEADS.BACKWARD_SCALE
+            features_de_rcnn = {k: self.affine_rcnn(decouple_layer(features[k], scale)) for k in features}
+        results, detector_losses = self.roi_heads(images, features_de_rcnn, proposals, gt_instances)
+
+        return proposal_losses, detector_losses, results, images.image_sizes
+
+    def preprocess_image(self, batched_inputs):
+        images = [x["image"].to(self.device) for x in batched_inputs]
+        images = [self.normalizer(x) for x in images]
+        images = ImageList.from_tensors(images, self.backbone.size_divisibility)
+        return images
+
+    def normalize_fn(self):
+        assert len(self.cfg.MODEL.PIXEL_MEAN) == len(self.cfg.MODEL.PIXEL_STD)
+        num_channels = len(self.cfg.MODEL.PIXEL_MEAN)
+        pixel_mean = (torch.Tensor(
+            self.cfg.MODEL.PIXEL_MEAN).to(self.device).view(num_channels, 1, 1))
+        pixel_std = (torch.Tensor(
+            self.cfg.MODEL.PIXEL_STD).to(self.device).view(num_channels, 1, 1))
+        return lambda x: (x - pixel_mean) / pixel_std
